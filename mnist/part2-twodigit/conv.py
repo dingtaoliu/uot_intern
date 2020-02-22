afa8ceafaac8e395bf403edea7c62bdb08beac61
dingtaoliu <danny.dingtao.liu@gmail.com>
2020-02-22 13:07:34 -0500

    adding 3 CNN models

---
+++ mnist/part2-twodigit/conv.py
@@ -0,0 +1,99 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from train_utils import batchify_data, run_epoch, train_model, Flatten
+import utils_multiMNIST as U
+
+from conv_nets import *
+path_to_data_dir = '../Datasets/'
+use_mini_dataset = True
+
+batch_size = 32
+nb_classes = 10
+nb_epoch = 30
+num_classes = 10
+img_rows, img_cols = 42, 28 # input image dimensions
+
+
+# Baseline CNN model
+class CNN(nn.Module):
+
+    def __init__(self, input_dimension):
+        super(CNN, self).__init__()
+
+        # input layer: preserve the original dimensions
+        self.conv1 = nn.Conv2d(1, 32, 3, stride=1, padding=1)
+
+        # conv layer 
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
+
+        # conv layer
+        self.conv3 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
+
+        self.flatten = Flatten()
+
+        self.linear = nn.Linear(42 * 28 * 64, num_classes * 2)
+
+
+
+    def forward(self, x):
+
+        h = F.relu(self.conv1(x))
+        h = F.max_pool2d(h, kernel_size=3, stride=1, padding=1)
+        #print(h.shape)
+
+        h = F.relu(self.conv2(h))
+        h = F.max_pool2d(h, kernel_size=3, stride=1, padding=1)
+        #print(h.shape)
+
+        h = F.relu(self.conv3(h))
+        h = F.max_pool2d(h, kernel_size=3, stride=1, padding=1)
+
+        h_flat = self.flatten(h)
+
+        logits = self.linear(h_flat)
+
+        out_first_digit = F.softmax(logits[:, :num_classes], dim=1)
+        out_second_digit = F.softmax(logits[:, num_classes:], dim=1)
+
+        return out_first_digit, out_second_digit
+
+
+
+def main():
+    X_train, y_train, X_test, y_test = U.get_data(path_to_data_dir, use_mini_dataset)
+
+    # Split into train and dev
+    dev_split_index = int(9 * len(X_train) / 10)
+    X_dev = X_train[dev_split_index:]
+    y_dev = [y_train[0][dev_split_index:], y_train[1][dev_split_index:]]
+    X_train = X_train[:dev_split_index]
+    y_train = [y_train[0][:dev_split_index], y_train[1][:dev_split_index]]
+
+    permutation = np.array([i for i in range(len(X_train))])
+    np.random.shuffle(permutation)
+    X_train = [X_train[i] for i in permutation]
+    y_train = [[y_train[0][i] for i in permutation], [y_train[1][i] for i in permutation]]
+
+    # Split dataset into batches
+    train_batches = batchify_data(X_train, y_train, batch_size)
+    dev_batches = batchify_data(X_dev, y_dev, batch_size)
+    test_batches = batchify_data(X_test, y_test, batch_size)
+
+    # Load model
+    input_dimension = img_rows * img_cols
+    model = SCNN2(input_dimension) # TODO add proper layers to CNN class above
+
+    # Train
+    train_model(train_batches, dev_batches, model, n_epochs=20)
+
+    ## Evaluate the model on test data
+    loss, acc = run_epoch(test_batches, model.eval(), None)
+    print('Test loss1: {:.6f}  accuracy1: {:.6f}  loss2: {:.6f}   accuracy2: {:.6f}'.format(loss[0], acc[0], loss[1], acc[1]))
+
+if __name__ == '__main__':
+    # Specify seed for deterministic behavior, then shuffle. Do not change seed for official submissions to edx
+    np.random.seed(12321)  # for reproducibility
+    torch.manual_seed(12321)  # for reproducibility
+    main()
---
+++ mnist/part2-twodigit/conv_nets.py
@@ -0,0 +1,166 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from train_utils import Flatten
+
+num_classes = 10
+
+
+# Split CNN model
+class SCNN(nn.Module):
+
+    def __init__(self, input_dimension):
+        super(SCNN, self).__init__()
+
+        # input layer: preserve the original dimensions
+        self.conv1_1 = nn.Conv2d(1, 32, 3, stride=1, padding=1)
+        self.conv1_2 = nn.Conv2d(1, 32, 3, stride=1, padding=1)
+
+        # conv layer 
+        self.conv2_1 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
+        self.conv2_2 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
+
+        self.flatten = Flatten()
+
+        self.linear1 = nn.Linear(28 * 28 * 64, num_classes)
+        self.linear2 = nn.Linear(28 * 28 * 64, num_classes)
+
+
+
+    def forward(self, x):
+
+        h1 = F.relu(self.conv1_1(x[:, :, :28, :]))
+        h1 = F.max_pool2d(h1, kernel_size=3, stride=1, padding=1)
+
+        h2 = F.relu(self.conv1_2(x[:, :, 14:, :]))
+        h2 = F.max_pool2d(h2, kernel_size=3, stride=1, padding=1)
+
+        h1 = F.relu(self.conv2_1(h1))
+        h1 = F.max_pool2d(h1, kernel_size=3, stride=1, padding=1)
+
+        h2 = F.relu(self.conv2_2(h2))
+        h2 = F.max_pool2d(h2, kernel_size=3, stride=1, padding=1)
+
+        h1_flat = self.flatten(h1)
+        h2_flat = self.flatten(h2)
+
+        logits1 = self.linear1(h1_flat)
+        logits2 = self.linear2(h2_flat)
+
+        out_first_digit = F.softmax(logits1, dim=1)
+        out_second_digit = F.softmax(logits2, dim=1)
+
+        return out_first_digit, out_second_digit
+
+
+# Split CNN model
+class SCNN2(nn.Module):
+
+    def __init__(self, input_dimension):
+        super(SCNN2, self).__init__()
+
+        # input layer: preserve the original dimensions
+        self.conv1 = nn.Conv2d(1, 32, 3, stride=1, padding=1)
+
+        # conv layer 
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
+
+        self.conv3 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
+
+        self.flatten = Flatten()
+
+        self.linear1 = nn.Linear(14 * 14 * 64, 128)
+        self.linear2 = nn.Linear(128, num_classes)
+
+        num_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        print("Model contains {} trainable parameters".format(num_params))
+
+
+
+    def forward(self, x):
+
+        h1 = F.relu(self.conv1(x[:, :, :28, :]))
+        h1 = F.max_pool2d(h1, kernel_size=3, stride=1, padding=1)
+
+        h2 = F.relu(self.conv1(x[:, :, 14:, :]))
+        h2 = F.max_pool2d(h2, kernel_size=3, stride=1, padding=1)
+
+        h1 = F.relu(self.conv2(h1))
+        h1 = F.max_pool2d(h1, kernel_size=2, stride=2, padding=0)
+
+        h2 = F.relu(self.conv2(h2))
+        h2 = F.max_pool2d(h2, kernel_size=2, stride=2, padding=0)
+
+        h1 = F.relu(self.conv3(h1))
+        h2 = F.relu(self.conv3(h2))
+
+        h1 = self.flatten(h1)
+        h2 = self.flatten(h2)
+
+        h1 = torch.sigmoid(self.linear1(h1))
+        h2 = torch.sigmoid(self.linear1(h2))
+
+        logits1 = self.linear2(h1)
+        logits2 = self.linear2(h2)
+
+        out_first_digit = F.softmax(logits1, dim=1)
+        out_second_digit = F.softmax(logits2, dim=1)
+
+        return out_first_digit, out_second_digit
+
+# Split CNN model
+class SCNN3(nn.Module):
+
+    def __init__(self, input_dimension):
+        super(SCNN3, self).__init__()
+
+        # depthwise separable layer 1
+        self.conv1 = nn.Conv2d(1, 32, 3, stride=1, padding=1)
+
+        # depthwise separable layer 2
+        self.depthwise1 = nn.Conv2d(32, 32, 3, padding=1, groups=32)
+        self.pointwise1 = nn.Conv2d(32, 64, 1)
+
+        # depthwise separable layer 3
+        self.depthwise2 = nn.Conv2d(64, 64, 3, padding=1, groups=64)
+        self.pointwise2 = nn.Conv2d(64, 128, 1)
+
+        self.flatten = Flatten()
+
+        self.linear = nn.Linear(14 * 14 * 128, num_classes)
+
+        num_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
+        print("Model contains {} trainable parameters".format(num_params))
+
+
+
+    def forward(self, x):
+        h1 = F.relu(self.conv1(x[:, :, :28, :]))
+        h1 = F.max_pool2d(h1, kernel_size=3, stride=1, padding=1)
+
+        h2 = F.relu(self.conv1(x[:, :, 14:, :]))
+        h2 = F.max_pool2d(h2, kernel_size=3, stride=1, padding=1)
+
+        h1 = F.relu(self.pointwise1(self.depthwise1(torch.cat((h1, x), dim=1))))
+        h1 = F.max_pool2d(h1, kernel_size=2, stride=2)
+        #print(h1.shape)
+
+        h2 = F.relu(self.pointwise1(self.depthwise1(torch.cat((h2, x), dim=1))))
+        h2 = F.max_pool2d(h2, kernel_size=2, stride=2)
+        #print(h2.shape)
+
+        h1 = F.relu(self.pointwise2(self.depthwise2(h1)))
+
+        h2 = F.relu(self.pointwise2(self.depthwise2(h2)))
+
+        h1_flat = self.flatten(h1)
+        h2_flat = self.flatten(h2)
+
+        logits1 = self.linear(h1_flat)
+        logits2 = self.linear(h2_flat)
+
+        out_first_digit = F.softmax(logits1, dim=1)
+        out_second_digit = F.softmax(logits2, dim=1)
+
+        return out_first_digit, out_second_digit
